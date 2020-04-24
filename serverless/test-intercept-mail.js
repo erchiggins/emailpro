0db226499a95ac313bf191b459d88a0972427430
Emily Higgins <emilyrchiggins@gmail.com>
2020-04-24 13:18:32 -0400

    successful archive update and date parsing for forwarded and direct messages

--- serverless/test-intercept-mail.js
+++ serverless/test-intercept-mail.js
@@ -1,6 +1,5 @@
 const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB();
-const receiver = 'bot@screenforce-dev.com';
 
 exports.handler = async(event) => {
     const rawMessage = event.Records[0].Sns.Message;
@@ -15,22 +14,19 @@ exports.handler = async(event) => {
         const rawContent = message.content;
         const content = decode(rawContent);
         const item = {
-            "timestamp": { "S": content.date.timestamp },
-            "subject": { "S": subject },
-            "sender": { "S": sender },
-            "plaintext": { "SS": Array.from(content.plaintext) },
-            "markdown": { "S": content.markdown }
+            "timestamp": { S: content.date.timestamp },
+            "subject": { S: subject },
+            "sender": { S: sender },
+            "plaintext": { SS: Array.from(content.plaintext) },
+            "markdown": { S: content.markdown }
         };
-        console.log(item);
         const archiveItem = {
-            "timestamp": { "S": content.date.timestamp },
-            "subject": { "S": subject },
-            "year": { "N": content.date.year },
-            "month": { "S": content.date.month },
-            "day": { "N": content.date.day }
+            "timestamp": { S: content.date.timestamp },
+            "subject": { S: subject },
+            "year": { N: content.date.year + '' },
+            "month": { S: content.date.month },
+            "day": { N: content.date.day + '' }
         };
-        console.log(archiveItem);
-
         const saveparams = {
             TableName: "TestMail",
             Item: item,
@@ -43,7 +39,19 @@ exports.handler = async(event) => {
                 console.log(data);
             }
         }).promise();
-        // add archive table
+        const archiveparams = {
+            TableName: "TestArchive",
+            Item: archiveItem,
+            ReturnConsumedCapacity: "TOTAL"
+        };
+        const savedarchive = await ddb.putItem(archiveparams, function(err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(data);
+            }
+        }).promise();
+        // add topics table
     } else {
         return 'unrecognized sender';
     }
@@ -120,16 +128,37 @@ function decode(content) {
         const intval = parseInt(encoded, 16);
         return String.fromCharCode(intval);
     });
-    let chunks = [];
+    let timesent = "";
+    let body = "";
     if (mdString.includes('<div dir="ltr"')) {
-        chunks = mdString.split('<div dir="ltr"');
+        // indicates that message contains forwarded blocks
+        let chunks = mdString.split('<div dir="ltr"');
+        console.log(chunks);
+        timesent = chunks[chunks.length - 2];
+        const dateIndex = timesent.indexOf('<br>Date: ');
+        const subjIndex = timesent.indexOf('<br>Subject: ');
+        timesent = timesent.substring(dateIndex + 10, subjIndex);
+        body = chunks[chunks.length - 1].substring(0, mdString.length - ((chunks.length - 1) * 6));
+    } else {
+        timesent = lines[marks.plainStart - 6];
+        console.log(timesent);
+        timesent = timesent.substring(6);
+        body = mdString;
     }
-    let timeOfMail = chunks[chunks.length - 2];
-    const dateIndex = timeOfMail.indexOf('<br>Date: ');
-    const subjIndex = timeOfMail.indexOf('<br>Subject: ');
-    timeOfMail = timeOfMail.substring(dateIndex + 10, subjIndex);
-    timeOfMail = timeOfMail.replace(':', ' ');
-    let dateParts = timeOfMail.split(' ');
+    toReturn.date = processDate(timesent);
+    toReturn.markdown = body;
+    return toReturn;
+}
+
+function processDate(timestring) {
+    let timetoreturn = {
+        year: 0,
+        month: "",
+        day: 0,
+        timestamp: ""
+    }
+    timestring = timestring.replace(':', ' ');
+    let dateParts = timestring.split(' ');
     let year = parseInt(dateParts[3]);
     const months = {
         'Jan': 0,
@@ -145,21 +174,28 @@ function decode(content) {
         'Nov': 10,
         'Dec': 11
     };
-    let month = months[dateParts[1]];
-    let day = parseInt(dateParts[2].substring(0, dateParts[2].length - 1));
-    let hour = parseInt(dateParts[5]) + ((dateParts[7] === 'PM') ? 12 : 0);
-    let minute = parseInt(dateParts[6]);
+    // handle day/month inversion for different formats
+    let month, day, hour, minute;
+    if (dateParts[1] in months) {
+        month = months[dateParts[1]];
+        day = parseInt(dateParts[2].substring(0, dateParts[2].length - 1));
+        hour = parseInt(dateParts[5]) + ((dateParts[7] === 'PM') ? 12 : 0);
+        minute = parseInt(dateParts[6]);
+    } else {
+        month = months[dateParts[2]];
+        day = parseInt(dateParts[1]);
+        hour = parseInt(dateParts[4]);
+        minute = parseInt(dateParts[5].substring(0, 2));
+    }
     let date = new Date();
     date.setFullYear(year);
     date.setMonth(month);
     date.setDate(day);
     date.setHours(hour);
     date.setMinutes(minute);
-    toReturn.date.timestamp = date.getTime() + "";
-    toReturn.date.year = year;
-    toReturn.date.month = dateParts[1];
-    toReturn.date.day = day;
-    let body = chunks[chunks.length - 1].substring(0, mdString.length - ((chunks.length - 1) * 6));
-    toReturn.markdown = body;
-    return toReturn;
+    timetoreturn.timestamp = date.getTime() + "";
+    timetoreturn.year = year;
+    timetoreturn.month = dateParts[2];
+    timetoreturn.day = day;
+    return timetoreturn;
 }
